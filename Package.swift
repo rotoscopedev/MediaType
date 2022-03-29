3d6bc19e549bafa1a14f395012832f89b22d39c7
Simon Wilson <simon@rotoscope.dev>
2022-03-29 11:02:08 +0200

    Add IANACharset dependency

--- Package.swift
+++ Package.swift
@@ -14,15 +14,21 @@ let package = Package(
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     .package(url: "https://github.com/Quick/Nimble.git", from: "9.0.0"),
+    .package(url: "https://github.com/rotoscopedev/IANACharset.git", from: "0.9.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "MediaType",
-      dependencies: []),
+      dependencies: [
+        "IANACharset",
+      ]),
     .testTarget(
       name: "MediaTypeTests",
-      dependencies: ["MediaType", "Nimble"]),
+      dependencies: [
+        "MediaType",
+        "Nimble"
+      ]),
   ]
 )
