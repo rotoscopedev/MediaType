61dc5d7806d64ad7de4e62c12136800c181e7ecd
Simon Wilson <simon@rotoscope.dev>
2024-11-16 02:07:35 +0100

    Update platform versions

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:6.0
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "MediaType",
+  platforms: [
+    .macOS(.v11), .iOS(.v14), .tvOS(.v14), .watchOS(.v7), .visionOS(.v1),
+  ],
   products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
